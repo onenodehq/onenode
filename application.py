aa50c7f9ca7e4b8788ad729c346c4234d752acba
TomoKanazawa <itstomohiro@gmail.com>
2024-04-06 14:55:47 -0700

    Added test v1 endpoint

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from blueprints.v1.view import v1
 
 # Load environment variables
 load_dotenv()
@@ -10,10 +11,13 @@ llm = ChatOpenAI()
 # Flask application configuration
 application = Flask(__name__)
 
+# Register the Blueprint
+application.register_blueprint(v1, url_prefix='/v1')
+
 # Home route
 @application.route("/")
 def home():
-    res = llm.invoke("how can langsmith help with testing?")
+    res = llm.invoke("How to cook eggs?")
     return jsonify({"response": res.content})
 
 
--- blueprints/v1/view.py
+++ blueprints/v1/view.py
@@ -2,14 +2,6 @@ from flask import request, jsonify, current_app, Blueprint
 
 v1 = Blueprint('v1', __name__, url_prefix='/v1')
 
-@v1.route("/query", methods=["GET"])
-def query_index():
-    query_text = request.args.get("text", None)
-    if query_text is None:
-        return (
-            "No text found, please include a ?text=blah parameter in the URL",
-            200,
-        )
-    query_engine = current_app.config["INDEX"].as_query_engine()
-    response = query_engine.query(query_text)
-    return str(response), 200
+@v1.route("/test", methods=["GET"])
+def test():
+    return jsonify({"status": "success", "message": "Test endpoint reached"}), 200
