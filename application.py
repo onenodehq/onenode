675b890c70dfb7bfff4ddcd7c5c4aa9a947a05e3
TomoKanazawa <itstomohiro@gmail.com>
2024-04-08 00:16:03 -0700

    Added /add and /query blueprint to application.py

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from blueprints.v1.view import v1
+from blueprints.v1.add import v1_blueprint_add
 
 # Load environment variables
 load_dotenv()
@@ -12,7 +13,9 @@ llm = ChatOpenAI()
 application = Flask(__name__)
 
 # Register the Blueprint
-application.register_blueprint(v1, url_prefix='/v1')
+application.register_blueprint(v1)
+application.register_blueprint(v1_blueprint_add)
+
 
 # Home route
 @application.route("/")
