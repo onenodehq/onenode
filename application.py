4d7e5b7b29afde0cef269b7a7ce8d9024b22e3e1
TomoKanazawa <itstomohiro@gmail.com>
2024-05-03 15:09:23 -0700

    Changed home to hello world

--- application.py
+++ application.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from blueprints.v1.view import v1
-from blueprints.v1.add import v1_blueprint_add
 from blueprints.v1.query import v1_blueprint_query
 from blueprints.test.view import test_blueprint_query
 
@@ -16,7 +15,6 @@ application = Flask(__name__)
 
 # Register the Blueprint
 application.register_blueprint(v1)
-application.register_blueprint(v1_blueprint_add)
 application.register_blueprint(test_blueprint_query)
 application.register_blueprint(v1_blueprint_query)
 
@@ -24,8 +22,7 @@ application.register_blueprint(v1_blueprint_query)
 # Home route
 @application.route("/")
 def home():
-    res = llm.invoke("How to cook eggs?")
-    return jsonify({"response": res.content})
+    return 'Hello, World!'
 
 
 # Only for development environment
