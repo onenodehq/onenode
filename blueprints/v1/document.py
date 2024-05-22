19e19e337d0454b60fd6cc62d5cb2fcbe4c12dbd
TomoKanazawa <itstomohiro@gmail.com>
2024-05-21 18:31:06 -0700

    Completed basic implementation of v1/document POST

--- blueprints/v1/document.py
+++ blueprints/v1/document.py
@@ -10,7 +10,7 @@ v1_blueprint_document = Blueprint("document", __name__, url_prefix="/v1/document
 
 
 @v1_blueprint_document.route("/", methods=["GET"])
-@jwt_required
+@jwt_required()
 def get_document():
     ids = request.args.get("document_ids")
     where = request.args.get("where")
@@ -27,18 +27,31 @@ def get_document():
 
 
 @v1_blueprint_document.route("/", methods=["POST"])
-@jwt_required
+@jwt_required()
 def create_document():
     content_type = request.content_type
 
     # Handling JSON data
     if content_type == "application/json":
         data = request.get_json()
-        documents = data.documents
-        is_public = data.is_public
+        # Check for required fields
+        if not data:
+            return jsonify({"error": "No JSON data provided"}), 400
+
+        documents = data.get("documents")
+        user_id = data.get("user_id")
+        if not documents or not user_id:
+            return (
+                jsonify(
+                    {"error": "Missing required fields: 'documents' and 'user_id'"}
+                ),
+                400,
+            )
+
+        is_public = bool(data.get("is_public", False))
+        group_id = data.get("group_id", str(uuid.uuid4()))
         ids = [str(uuid.uuid4()) for _ in documents]
-        user_id = data.userId
-        created_at = datetime.datetime.now()
+        created_at = datetime.datetime.now(datetime.UTC).isoformat()
         metadatas = []
 
         for id in ids:
@@ -48,6 +61,8 @@ def create_document():
                     "user_id": user_id,
                     "is_public": is_public,
                     "created_at": created_at,
+                    "updated_at": created_at,
+                    "group_id": group_id,
                 }
             )
 
@@ -61,7 +76,12 @@ def create_document():
             ids=ids,
         )
 
-        return jsonify({"received": data}), 200
+        response = {
+            "documents": documents,
+            "metadatas": metadatas,
+        }
+
+        return jsonify(response), 200
         # Handling other content types
     else:
         return jsonify({"error": "Unsupported content type"}), 400
