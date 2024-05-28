a9ca81a9e8458b8bde91b100ac40d25cefce09e7
TomoKanazawa <itstomohiro@gmail.com>
2024-05-28 11:26:35 -0700

    Added exception handling at /document POST

--- blueprints/v1/document.py
+++ blueprints/v1/document.py
@@ -6,6 +6,7 @@ from config import get_db_path
 from auth.auth import jwt_required
 from blueprints.v1.utils.chroma_setup import (
     vectorstore,
+    collection,
 )  # Import the initialized components
 
 # Define a Blueprint for the '/v1/query' endpoint
@@ -27,59 +28,61 @@ def get_document():
 @v1_blueprint_document.route("/", methods=["POST"])
 @jwt_required
 def create_document():
-    content_type = request.content_type
+    try:
+        content_type = request.content_type
 
-    # Handling JSON data
-    if content_type == "application/json":
-        data = request.get_json()
-        # Check for required fields
-        if not data:
-            return jsonify({"error": "No JSON data provided"}), 400
+        # Handling JSON data
+        if content_type == "application/json":
+            data = request.get_json()
+            # Check for required fields
+            if not data:
+                return jsonify({"error": "No JSON data provided"}), 400
 
-        documents = data.get("contents")
-        user_id = data.get("user_id")
-        types = data.get("types")
-        if not documents or not user_id:
-            return (
-                jsonify(
-                    {"error": "Missing required fields: 'documents' and 'user_id'"}
-                ),
-                400,
+            documents = data.get("contents")
+            user_id = data.get("user_id")
+            types = data.get("types")
+            if not documents or not user_id:
+                return (
+                    jsonify(
+                        {"error": "Missing required fields: 'documents' and 'user_id'"}
+                    ),
+                    400,
+                )
+
+            is_public = bool(data.get("is_public", False))
+            group_id = data.get("group_id", str(uuid.uuid4()))
+            ids = [str(uuid.uuid4()) for _ in documents]
+            created_at = datetime.datetime.now(datetime.UTC).isoformat()
+            metadatas = []
+
+            for i, id in enumerate(ids):
+                metadatas.append(
+                    {
+                        "id": id,
+                        "type": types[i],
+                        "user_id": user_id,
+                        "is_public": str(is_public),
+                        "created_at": created_at,
+                        "updated_at": created_at,
+                        "group_id": group_id,
+                    }
+                )
+
+            collection.add(
+                documents=documents,
+                metadatas=metadatas,
+                ids=ids,
             )
 
-        is_public = bool(data.get("is_public", False))
-        group_id = data.get("group_id", str(uuid.uuid4()))
-        ids = [str(uuid.uuid4()) for _ in documents]
-        created_at = datetime.datetime.now(datetime.UTC).isoformat()
-        metadatas = []
+            response = {
+                "contents": documents,
+                "metadatas": metadatas,
+            }
 
-        for i, id in enumerate(ids):
-            metadatas.append(
-                {
-                    "id": id,
-                    "type": types[i],
-                    "user_id": user_id,
-                    "is_public": str(is_public),
-                    "created_at": created_at,
-                    "updated_at": created_at,
-                    "group_id": group_id,
-                }
-            )
+            return jsonify(response), 200
 
-        
+        else:
+            return jsonify({"error": "Unsupported content type"}), 400
 
-        vectorstore.add_documents(
-            documents=documents,
-            metadatas=metadatas,
-            ids=ids,
-        )
-
-        response = {
-            "contents": documents,
-            "metadatas": metadatas,
-        }
-
-        return jsonify(response), 200
-        # Handling other content types
-    else:
-        return jsonify({"error": "Unsupported content type"}), 400
+    except Exception as e:
+        return jsonify({"error": "An error occurred", "details": str(e)}), 500
