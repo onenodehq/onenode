65175a1ed266861b935364266797dbdd69a28f43
TomoKanazawa <itstomohiro@gmail.com>
2024-06-26 00:25:57 -0700

    Created imageToText()

--- api/openai/imageToText.py
+++ api/openai/imageToText.py
@@ -1,39 +1,69 @@
-import base64
-import os
-import aiohttp
-import asyncio
+from openai import OpenAI
+from typing import Dict
 
-# OpenAI API Key
-api_key = os.environ.get("OPENAI_API_KEY")
-
-headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
+from typeguard import typechecked
 
 
-async def imageToText(base64_image):
-    payload = {
-        "model": "gpt-4o",
-        "messages": [
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": "Give a comprehensive description about this image.",
+@typechecked
+def image_to_text(image_url: str) -> str:
+    """
+    Analyze a list of image URLs and return the OpenAI API response.
+
+    Args:
+        image_urls (List[str]): A list of image URLs to be analyzed.
+
+    Returns:
+        Dict: The response from the OpenAI API.
+    """
+    client = OpenAI()
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What’s in this image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": image_url,
+                        "detail": "high",
                     },
-                    {
-                        "type": "image_url",
-                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
-                    },
-                ],
+                },
+            ],
+        }
+    ]
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=messages,
+        max_tokens=300,
+    )
+
+    print(response)
+
+    return response.choices[0].message.content
+
+""" response = {
+    'id': 'chatcmpl-9eFt4JjhosRB7UvpARb946lgQc8me',
+    'choices': [
+        {
+            'finish_reason': 'stop',
+            'index': 0,
+            'logprobs': None,
+            'message': {
+                'content': 'The image shows a table setup that includes several wine glasses, each filled partially with red wine. The glasses are arranged neatly and some are placed on coasters that have the text "Opus One" printed on them. There are also a couple of white napkins on the table with the same text "Opus One." Additionally, there is a single glass of water present among the wine glasses. The setting appears to be arranged for a wine tasting or a similar event.',
+                'role': 'assistant',
+                'function_call': None,
+                'tool_calls': None
             }
-        ],
-        "max_tokens": 300,
+        }
+    ],
+    'created': 1719381218,
+    'model': 'gpt-4o-2024-05-13',
+    'object': 'chat.completion',
+    'system_fingerprint': 'fp_4008e3b719',
+    'usage': {
+        'completion_tokens': 95,
+        'prompt_tokens': 778,
+        'total_tokens': 873
     }
-
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            "https://api.openai.com/v1/chat/completions", headers=headers, json=payload
-        ) as response:
-            response_json = await response.json()
-            print(response_json)
-            return response_json
+} """
