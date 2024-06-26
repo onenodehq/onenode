a9c0e214066e0f936598cf32a225773238dc306c
TomoKanazawa <itstomohiro@gmail.com>
2024-06-26 00:24:56 -0700

    Created s3_setup.py

---
+++ blueprints/v1/utils/s3_setup.py
@@ -0,0 +1,28 @@
+import os
+import boto3
+
+# Configure the S3 client
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("COGNITO_AWS_REGION"),
+)
+
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+
+
+# Define the MIME type extension map globally
+EXTENSION_MAP = {
+    "image/jpeg": "jpg",
+    "image/png": "png",
+    "image/gif": "gif",
+    "image/bmp": "bmp",
+    "image/tiff": "tiff",
+    "image/webp": "webp",
+    "image/svg+xml": "svg",
+    "image/x-icon": "ico",
+    "image/heif": "heif",
+    "image/heic": "heic",
+    "image/avif": "avif",
+}
