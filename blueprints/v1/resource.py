8978ccb2c5db87925e66cfb01cd86bb7f4e8513a
TomoKanazawa <itstomohiro@gmail.com>
2024-06-26 00:26:29 -0700

    Added image saving logic both Pinecone and S3

--- blueprints/v1/resource.py
+++ blueprints/v1/resource.py
@@ -1,20 +1,23 @@
-import asyncio
+import base64
 import datetime
+from io import BytesIO
+import io
 import os
 import re
 from typing import Dict, List
 import uuid
-from dataclasses_json import dataclass_json
 from dotenv import load_dotenv
 from flask import Blueprint, jsonify, request
-from api.openai.imageToText import imageToText
-from config import get_db_path
+from typeguard import typechecked
+from api.openai.imageToText import image_to_text
+from api.openai.s3 import generate_signed_url, upload_to_s3
 from auth.auth import jwt_required
 from blueprints.v1.utils.pinecone_setup import (
     vectorstore,
     openai_ef,
     index,
 )  # Import the initialized components
+from blueprints.v1.utils.s3_setup import EXTENSION_MAP, S3_BUCKET_NAME
 from langchain.schema import Document
 import logging
 
@@ -82,58 +85,66 @@ def create_resource(user_id):
     try:
         content_type = request.content_type
 
-        # Handling JSON data
-        if content_type == "application/json":
-            data = request.get_json()
-            # Check for required fields
-            if not data:
-                return jsonify({"error": "No JSON data provided"}), 400
-
-            resources = data.get("resources")
-            if not resources:
-                return jsonify({"error": "No resources provided"}), 400
-
-            ids = [str(uuid.uuid4()) for _ in resources]
-            created_at = datetime.datetime.now(datetime.UTC).isoformat()
-            documents: List[Document] = []
-            response: List[dict] = []
-
-            for i, resource in enumerate(resources):
-                metadata = resource.get("metadata")
-                metadata_snake_case = convert_keys_to_snake_case(metadata)
-                metadata_snake_case.update(
-                    {
-                        "id": ids[i],
-                        "created_at": created_at,
-                        "updated_at": created_at,
-                    }
-                )
-
-                if metadata.get("type") == "image":
-                    base64_image = resource.get("content")
-                    content = asyncio.run(imageToText(base64_image))
-                else:
-                    content = resource.get("content")
-                document = Document(
-                    metadata=metadata_snake_case,
-                    page_content=content,
-                )
-                documents.append(document)
-
-                response_item: dict = {}
-                response_item["content"] = content
-                response_item["metadata"] = metadata_snake_case
-                response.append(response_item)
-
-            vectorstore.add_documents(documents=documents, ids=ids)
-
-            return jsonify(response), 200
-
-        else:
+        if content_type != "application/json":
             return jsonify({"error": "Unsupported content type"}), 400
 
+        data = request.get_json()
+        if not data:
+            return jsonify({"error": "No JSON data provided"}), 400
+
+        resources = data.get("resources")
+        if not resources:
+            return jsonify({"error": "No resources provided"}), 400
+
+        ids = [str(uuid.uuid4()) for _ in resources]
+        created_at = datetime.datetime.now().isoformat()
+
+        documents: List[Document] = []
+        response: List[dict] = []
+
+        resources = process_image_resources(resources=resources, ids=ids)
+
+        for i, resource in enumerate(resources):
+            metadata = resource.get("metadata")
+            content = resource.get("content")
+            if not metadata:
+                return jsonify({"error": f"Metadata missing for resource {i}"}), 400
+
+            metadata_snake_case = convert_keys_to_snake_case(metadata)
+            metadata_snake_case.update(
+                {
+                    "id": ids[i],
+                    "created_at": created_at,
+                    "updated_at": created_at,
+                    "user_id": user_id,
+                }
+            )
+
+            print("metadata,", metadata_snake_case)
+
+            document = Document(
+                metadata=metadata_snake_case,
+                page_content=content,
+            )
+            documents.append(document)
+            response_metadata = metadata_snake_case.copy()
+            response_metadata.update(
+                {"s3_key": generate_signed_url(metadata_snake_case["s3_key"])}
+            )
+            response_item = {
+                "content": content,
+                "metadata": response_metadata,
+            }
+            print("s3ke", metadata_snake_case["s3_key"])
+            response.append(response_item)
+            print("response", response)
+
+        vectorstore.add_documents(documents=documents, ids=ids)
+        return jsonify(response), 200
+
     except Exception as e:
-        print(e)
+        logging.error(f"Error saving resource: {e}")
+        # Consider using proper logging here
         return jsonify({"error": "An error occurred", "details": str(e)}), 500
 
 
@@ -224,3 +235,69 @@ def delete_resource(user_id):
     except Exception as e:
         logging.error(f"Error deleting resource: {e}")
         return jsonify({"error": "An error occurred", "details": str(e)}), 500
+
+
+@typechecked
+def process_image_resources(resources: List[Dict], ids: List[str]) -> List[Dict]:
+    updated_resources = []
+
+    for i, resource in enumerate(resources):
+        try:
+            metadata = resource.get("metadata", {})
+            mime_type = metadata.get("type")
+            if not mime_type:
+                raise ValueError("Missing MIME type for image")
+            if mime_type.startswith("image/"):
+                base64_image: str = resource.get("content")
+                if not base64_image or not isinstance(base64_image, str):
+                    raise ValueError("Content for image must be a base64 string")
+
+                # Analyze the images using the new analyze_images function
+                print("call api \n")
+                content = image_to_text(base64_image)
+
+                print("iamge to text result", content)
+
+                extension = EXTENSION_MAP.get(mime_type, "bin")
+                filename = f"{ids[i]}.{extension}"
+                save_base64_image(base64_image=base64_image, filename=filename)
+
+                s3_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{filename}"
+
+                metadata["s3_key"] = filename
+                resource["metadata"] = metadata
+                resource["content"] = content
+
+            updated_resources.append(resource)
+
+        except Exception as e:
+            logging.error(f"Error processing image resource: {e}")
+            raise RuntimeError(f"Failed to process resource content: {str(e)}")
+
+    return updated_resources
+
+
+def save_base64_image(base64_image, filename):
+    try:
+        # Check if the base64 string has metadata and extract content type if available
+        if base64_image.startswith("data:"):
+            header, base64_image = base64_image.split(",", 1)
+            content_type = header.split(";")[0].split(":")[1]
+        else:
+            content_type = (
+                "application/octet-stream"  # Default content type if not specified
+            )
+
+        # Decode the base64 string
+        binary_image = base64.b64decode(base64_image)
+        file_binary = io.BytesIO(binary_image)
+
+        # Upload to S3
+        upload_to_s3(file_binary, filename, content_type)
+
+    except (base64.binascii.Error, ValueError) as e:
+        logging.error(f"Failed to process base64 image: {e}")
+        raise RuntimeError(f"Failed to save image to S3: {str(e)}")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
+        raise RuntimeError(f"Failed to save image to S3: {str(e)}")
