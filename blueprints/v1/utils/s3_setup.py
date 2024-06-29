ac4be0e6c6d2bf262bdb81fe1b28ba15299c46d0
TomoKanazawa <itstomohiro@gmail.com>
2024-06-28 21:09:37 -0700

    test

--- blueprints/v1/utils/s3_setup.py
+++ blueprints/v1/utils/s3_setup.py
@@ -4,8 +4,6 @@ import boto3
 # Configure the S3 client
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
-    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
     region_name=os.getenv("COGNITO_AWS_REGION"),
 )
 
