db6a94132ed991b375c2bb46194e104c2e6846b1
TomoKanazawa <itstomohiro@gmail.com>
2024-06-28 21:41:19 -0700

    Revomed AWS keys to access S3 - EB now has proper role to access S3

--- blueprints/v1/utils/s3_setup.py
+++ blueprints/v1/utils/s3_setup.py
@@ -1,11 +1,21 @@
 import os
 import boto3
 
-# Configure the S3 client
-s3 = boto3.client(
-    "s3",
-    region_name=os.getenv("COGNITO_AWS_REGION"),
-)
+# Check if running on localhost
+if os.getenv("FLASK_RUN_HOST", "") == "localhost":
+    # Use credentials for local development
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("COGNITO_AWS_REGION"),
+    )
+else:
+    # Use IAM roles in production
+    s3 = boto3.client(
+        "s3",
+        region_name=os.getenv("COGNITO_AWS_REGION"),
+    )
 
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
