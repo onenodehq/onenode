a1eefbe0153b27e69389a407a72a2c18abc2c08e
TomoKanazawa <itstomohiro@gmail.com>
2024-07-01 18:42:00 -0700

    Added 'mongo' prefix to mogo variables

--- blueprints/v1/utils/mongo_setup.py
+++ blueprints/v1/utils/mongo_setup.py
@@ -1,10 +1,11 @@
 import os
+import certifi
 from pymongo import MongoClient
 
 # MongoDB connection string
 MONGO_URI = os.getenv("MONGO_URI")
 
 # Initialize MongoDB client
-mongo_client = MongoClient(MONGO_URI)
-db = mongo_client.get_database("onenode_database_dev")
-collection = db.get_collection("content_collection")
+mongo_client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
+mongo_db = mongo_client.get_database("onenode_database_dev")
+mongo_collection = mongo_db.get_collection("content_collection")
