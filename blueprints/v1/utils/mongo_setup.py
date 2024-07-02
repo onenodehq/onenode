9db6a1e54a66c1fb82a9085fc79fb354a4a1814c
TomoKanazawa <itstomohiro@gmail.com>
2024-07-02 10:15:09 -0700

    Added env variable for mongo db/collection names

--- blueprints/v1/utils/mongo_setup.py
+++ blueprints/v1/utils/mongo_setup.py
@@ -4,8 +4,10 @@ from pymongo import MongoClient
 
 # MongoDB connection string
 MONGO_URI = os.getenv("MONGO_URI")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+MONGO_COLLECTION_NAME = os.getenv("MONGO_COLLECTION_NAME")
 
 # Initialize MongoDB client
 mongo_client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
-mongo_db = mongo_client.get_database("onenode_database_dev")
-mongo_collection = mongo_db.get_collection("content_collection")
+mongo_db = mongo_client.get_database(MONGO_DB_NAME)
+mongo_collection = mongo_db.get_collection(MONGO_COLLECTION_NAME)
