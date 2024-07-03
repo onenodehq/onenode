3f6ba3de60a385bcccfed8fc6b325f8b126d88ae
TomoKanazawa <itstomohiro@gmail.com>
2024-07-03 12:21:59 -0700

    DEBUG: fixed bug of docs_to_context()

--- blueprints/v1/utils/question_helper.py
+++ blueprints/v1/utils/question_helper.py
@@ -6,7 +6,7 @@ def docs_to_context(docs: list[dict]) -> str:
     formatted = []
     for doc in docs:
         # Ensure that 'metadata' and 'id', 'text' keys exist
-        doc_id = doc.get("metadata", {}).get("id")
+        doc_id = doc.get("id")
         doc_text = doc.get("text")
 
         if doc_id is None or doc_text is None:
