38aee517acca07df1447ec113dee880593905b9b
TomoKanazawa <itstomohiro@gmail.com>
2024-07-07 17:23:51 -0700

    Add new update_resource_connection_service()

--- blueprints/v1/resource.py
+++ blueprints/v1/resource.py
@@ -3,7 +3,13 @@ from flask import Blueprint, jsonify, request
 from auth.auth import jwt_required
 import logging
 
-from blueprints.v1.services.resource_service import create_resource_service, delete_resource_service, get_resource_service, update_resource_service
+from blueprints.v1.services.resource_service import (
+    create_resource_service,
+    delete_resource_service,
+    get_resource_service,
+    update_resource_connection_service,
+    update_resource_service,
+)
 
 load_dotenv()
 
@@ -52,4 +58,17 @@ def delete_resource(user_id):
         return jsonify(response), 200
     except Exception as e:
         logging.error(f"Error deleting resource: {e}")
+        return jsonify({"error": "An error occurred", "details": str(e)}), 500
+
+
+@v1_blueprint_resource.route("/<id>/connection", methods=["PUT"])
+@jwt_required
+def update_connections(user_id, id):
+    try:
+        response = update_resource_connection_service(
+            user_id=user_id, request=request, main_id=id
+        )
+        return jsonify(response), 200
+    except Exception as e:
+        logging.error(f"Error updating connections: {e}")
         return jsonify({"error": "An error occurred", "details": str(e)}), 500
