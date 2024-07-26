64d2b410a7dfd2198c6c27781ea45547efdbd1db
TomoKanazawa <itstomohiro@gmail.com>
2024-07-25 17:18:09 -0700

    Add mongo_email_collection to mongo_setup

--- blueprints/v1/utils/mongo_setup.py
+++ blueprints/v1/utils/mongo_setup.py
@@ -6,8 +6,10 @@ from pymongo import MongoClient
 MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 MONGO_COLLECTION_NAME = os.getenv("MONGO_COLLECTION_NAME")
+MONGO_EMAIL_COLLECTION = os.getenv("MONGO_EMAIL_COLLECTION")
 
 # Initialize MongoDB client
 mongo_client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 mongo_db = mongo_client.get_database(MONGO_DB_NAME)
 mongo_collection = mongo_db.get_collection(MONGO_COLLECTION_NAME)
+mongo_email_collection = mongo_db.get_collection(MONGO_EMAIL_COLLECTION)
