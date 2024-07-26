a430a2c8355d875908744506b1d9dce9c31d5e10
TomoKanazawa <itstomohiro@gmail.com>
2024-07-26 07:41:24 -0700

    Add mongo_api_key_collection to setup file

--- blueprints/v1/utils/mongo_setup.py
+++ blueprints/v1/utils/mongo_setup.py
@@ -7,9 +7,11 @@ MONGO_URI = os.getenv("MONGO_URI")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 MONGO_COLLECTION_NAME = os.getenv("MONGO_COLLECTION_NAME")
 MONGO_EMAIL_COLLECTION = os.getenv("MONGO_EMAIL_COLLECTION")
+MONGO_API_KEY_COLLECTION = os.getenv("MONGO_API_KEY_COLLECTION")
 
 # Initialize MongoDB client
 mongo_client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 mongo_db = mongo_client.get_database(MONGO_DB_NAME)
 mongo_collection = mongo_db.get_collection(MONGO_COLLECTION_NAME)
 mongo_email_collection = mongo_db.get_collection(MONGO_EMAIL_COLLECTION)
+mongo_api_key_collection = mongo_db.get_collection(MONGO_API_KEY_COLLECTION)
