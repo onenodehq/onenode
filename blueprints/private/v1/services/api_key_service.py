83e6abd227225f8dc4e5b61feab3893c992bece1
TomoKanazawa <itstomohiro@gmail.com>
2024-07-25 18:45:01 -0700

    Add generate_api_key in api_key_service file

---
+++ blueprints/private/v1/services/api_key_service.py
@@ -0,0 +1,7 @@
+import secrets
+import string
+
+def generate_api_key(length=64):
+    alphabet = string.ascii_letters + string.digits
+    api_key = ''.join(secrets.choice(alphabet) for _ in range(length))
+    return api_key
