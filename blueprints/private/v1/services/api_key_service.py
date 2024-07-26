65032c76e4cc29a938ce9a9502e32ac1b17c6e9d
TomoKanazawa <itstomohiro@gmail.com>
2024-07-26 07:41:51 -0700

    Add hashing to api generation

--- blueprints/private/v1/services/api_key_service.py
+++ blueprints/private/v1/services/api_key_service.py
@@ -1,7 +1,27 @@
+import hashlib
 import secrets
 import string
+from blueprints.v1.utils.mongo_setup import mongo_api_key_collection
 
-def generate_api_key(length=64):
+
+def generate_api_key(length=64) -> string:
     alphabet = string.ascii_letters + string.digits
-    api_key = ''.join(secrets.choice(alphabet) for _ in range(length))
+    api_key = "".join(secrets.choice(alphabet) for _ in range(length))
     return api_key
+
+
+def hash_api_key(api_key: string) -> string:
+    # Create a SHA-256 hash object
+    hash_object = hashlib.sha256()
+    # Get the hexadecimal representation of the hash
+    hashed_api_key = hash_object.hexdigest()
+    return hashed_api_key
+
+
+def save_api_key(hased_api_key: string, onenode_id: string) -> string:
+    try:
+        mongo_api_key_collection.insert_one(
+            {"_id": hased_api_key, "onenode_id": onenode_id}
+        )
+    except Exception as e:
+        raise e
