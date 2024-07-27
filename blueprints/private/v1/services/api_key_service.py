e6e3078e78bf29da45e8e5878e2f98d7df48749f
TomoKanazawa <itstomohiro@gmail.com>
2024-07-26 23:13:37 -0700

    FIx api key generation code

--- blueprints/private/v1/services/api_key_service.py
+++ blueprints/private/v1/services/api_key_service.py
@@ -1,24 +1,32 @@
 import hashlib
+import os
 import secrets
 import string
 from blueprints.v1.utils.mongo_setup import mongo_api_key_collection
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ONENODE_API_KEY_LENGTH = int(os.getenv("ONENODE_API_KEY_LENGTH"))
 
 
-def generate_api_key(length=64) -> string:
+def generate_api_key(length=ONENODE_API_KEY_LENGTH) -> str:
     alphabet = string.ascii_letters + string.digits
     api_key = "".join(secrets.choice(alphabet) for _ in range(length))
     return api_key
 
 
-def hash_api_key(api_key: string) -> string:
+def hash_api_key(api_key: str) -> str:
     # Create a SHA-256 hash object
     hash_object = hashlib.sha256()
+    # Update the hash object with the bytes of the API key
+    hash_object.update(api_key.encode("utf-8"))
     # Get the hexadecimal representation of the hash
     hashed_api_key = hash_object.hexdigest()
     return hashed_api_key
 
 
-def save_api_key(hased_api_key: string, onenode_id: string) -> string:
+def save_api_key(hased_api_key: str, onenode_id: str) -> str:
     try:
         mongo_api_key_collection.insert_one(
             {"_id": hased_api_key, "onenode_id": onenode_id}
