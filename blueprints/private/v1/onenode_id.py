5095faddd3a07770230575d8b8720759ec78e4ed
TomoKanazawa <itstomohiro@gmail.com>
2024-07-27 10:56:42 -0700

    Debug: Fix onenode_id endpoint codes

--- blueprints/private/v1/onenode_id.py
+++ blueprints/private/v1/onenode_id.py
@@ -1,6 +1,6 @@
 import logging
-from flask import Blueprint, jsonify
-from auth.api_key_decorator import require_api_key
+from flask import Blueprint, jsonify, request
+from auth.api_key_decorator import require_admin_api_key
 from blueprints.private.v1.services.onenode_id_service import (
     get_or_create_onenode_id_service,
 )
@@ -11,12 +11,14 @@ private_v1_blueprint_onenode_id = Blueprint(
 )
 
 @private_v1_blueprint_onenode_id.route("", methods=["PUT"])
-@require_api_key
+@require_admin_api_key
 def get_create_user_id():
     # create new email (mongo generates _id which will be onenode_user_id in auth0)
     try:
-        response = get_or_create_onenode_id_service()
-        return jsonify(response), 200
+        data = request.get_json()
+        email = data.get("email")
+        onenode_id = get_or_create_onenode_id_service(email=email)
+        return jsonify({"onenode_id": onenode_id}), 200
     except Exception as e:
-        logging.error(f"Error saving resource: {e}")
-        return jsonify({"error": "An error occurred", "details": str(e)}), 500
+        logging.error(f"Error saving getting onenode_id: {e}")
+        return jsonify({"error": "An error occurred"}), 500
--- blueprints/private/v1/services/onenode_id_service.py
+++ blueprints/private/v1/services/onenode_id_service.py
@@ -1,19 +1,23 @@
 # email and user_id relation service
 
+from uuid import uuid4
 from flask import g
+from blueprints.private.v1 import onenode_id
 from blueprints.v1.utils.mongo_setup import mongo_email_collection
 
 
-def get_or_create_onenode_id_service():
+def get_or_create_onenode_id_service(email: str):
     try:
-        email = g.user.get("email")
-
         item = mongo_email_collection.find_one({"email": email})
+        print("mongo", item)
         if item:
-            user_id = item.get("_id")
-            return user_id
+            onenode_id = item.get("_id")
+            return onenode_id
         else:
-            new_item = mongo_email_collection.insert_one({"email": email})
-            user_id = new_item.inserted_id
+            new_item = mongo_email_collection.insert_one(
+                {"_id": str(uuid4()), "email": email}
+            )
+            onenode_id = new_item.inserted_id
+            return onenode_id
     except Exception as e:
         raise e
