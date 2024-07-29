cb447fe341662b702a9a8efcd2e21d2eeb17f735
TomoKanazawa <itstomohiro@gmail.com>
2024-07-29 09:43:41 -0700

    Change default org creation endpoint

--- blueprints/private/v1/org.py
+++ blueprints/private/v1/org.py
@@ -1,15 +1,16 @@
 import logging
 from flask import Blueprint, jsonify, request
 from auth.api_key_decorator import require_admin_api_key
+from auth.auth_decorator import requires_auth
 from blueprints.private.v1.services.org_service import get_or_create_org_and_project
 
 
 private_v1_blueprint_org = Blueprint(
-    "org", __name__, url_prefix="/private/v1/default-org"
+    "org", __name__, url_prefix="/private/v1/org"
 )
 
 
-@private_v1_blueprint_org.route("", methods=["PUT"])
+@private_v1_blueprint_org.route("/default", methods=["PUT"])
 @require_admin_api_key
 def get_or_create_org():
     try:
@@ -18,5 +19,5 @@ def get_or_create_org():
         get_or_create_org_and_project(onenode_id)
         return 200
     except Exception as e:
-        logging.error(f"Error saving resource: {e}")
+        logging.error(f"Error checking default org: {e}")
         return jsonify({"error": "An error occurred", "details": str(e)}), 500
