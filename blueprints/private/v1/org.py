bc50ef4b0fe508d06618747d0de82f0d72ca5734
TomoKanazawa <itstomohiro@gmail.com>
2024-07-30 13:05:49 -0700

    Add GET orgs endpoint

--- blueprints/private/v1/org.py
+++ blueprints/private/v1/org.py
@@ -1,13 +1,16 @@
 import logging
 from flask import Blueprint, jsonify, request
+from typeguard import typechecked
 from auth.api_key_decorator import require_admin_api_key
 from auth.auth_decorator import requires_auth
-from blueprints.private.v1.services.org_service import get_or_create_org_and_project
-
-
-private_v1_blueprint_org = Blueprint(
-    "org", __name__, url_prefix="/private/v1/org"
+from blueprints.private.v1.services.org_service import (
+    get_or_create_org_and_project,
+    get_orgs_and_projects_from_db,
 )
+from bson import json_util
+
+
+private_v1_blueprint_org = Blueprint("org", __name__, url_prefix="/private/v1/org")
 
 
 @private_v1_blueprint_org.route("/default", methods=["PUT"])
@@ -20,4 +23,16 @@ def get_or_create_org():
         return 200
     except Exception as e:
         logging.error(f"Error checking default org: {e}")
+        return jsonify({"error": "An error occurred", "details": str(e)}), 500
+
+
+@private_v1_blueprint_org.route("", methods=["GET"])
+@requires_auth
+@typechecked
+def get_orgs_and_projects():
+    try:
+        orgs_and_projects: list = get_orgs_and_projects_from_db()
+        return json_util.dumps(orgs_and_projects), 200
+    except Exception as e:
+        logging.error(f"Error creating org: {e}")
         return jsonify({"error": "An error occurred", "details": str(e)}), 500
