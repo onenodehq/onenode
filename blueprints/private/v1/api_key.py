f442aa641784940513a0d053960e72f1be5ef4eb
TomoKanazawa <itstomohiro@gmail.com>
2024-08-03 07:02:56 +0900

    Add hash value to new api key return value

--- blueprints/private/v1/api_key.py
+++ blueprints/private/v1/api_key.py
@@ -22,14 +22,12 @@ def create_api_key():
     onenode_id = g.onenode_id
     hashed_api_key = hash_api_key(api_key=api_key)
     save_api_key(hased_api_key=hashed_api_key, onenode_id=onenode_id, name=name)
-    return jsonify({"name": name, "value": api_key}), 200
+    return jsonify({"name": name, "value": api_key, "hash_value": hashed_api_key}), 200
 
 
 @private_v1_blueprint_api_key.route("", methods=["GET"])
 @requires_auth
 def get_api_key_metadata():
     onenode_id = g.onenode_id
-    print("onenode_id", onenode_id)
     api_key_metadata = get_api_key_metadata_from_db(onenode_id=onenode_id)
-    print("result\n\n\n", api_key_metadata)
     return jsonify(api_key_metadata), 200
