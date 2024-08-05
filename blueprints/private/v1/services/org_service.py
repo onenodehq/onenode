04d40c1948d1ac10c7bc2601ab7d23c10e71ac8f
TomoKanazawa <itstomohiro@gmail.com>
2024-08-05 21:47:14 +0900

    Rename collection to index in org_service

--- blueprints/private/v1/services/org_service.py
+++ blueprints/private/v1/services/org_service.py
@@ -8,7 +8,7 @@ from blueprints.v1.utils.mongo_setup import (
 def get_or_create_org_and_project(onenode_id: str):
     org = mongo_org_collection.find_one({"members": {"$in": [onenode_id]}})
     if not org:
-        new_project = mongo_project_collection.insert_one({"collections": []})
+        new_project = mongo_project_collection.insert_one({"indexes": []})
         new_project_id = new_project.inserted_id
         mongo_org_collection.insert_one(
             {"members": [onenode_id], "projects": [new_project_id]}
@@ -29,9 +29,7 @@ def get_orgs_and_projects_from_db():
         if not org.get("name"):
             org["name"] = "Default Organization"
         project_ids = org.get("projects")
-        projects_cursor = mongo_project_collection.find(
-            {"_id": {"$in": project_ids}}, {"_id": 0}
-        )
+        projects_cursor = mongo_project_collection.find({"_id": {"$in": project_ids}})
         projects = list(projects_cursor)
 
         for project in projects:
@@ -41,9 +39,12 @@ def get_orgs_and_projects_from_db():
 
         result.append(
             {
-                "id": org["_id"],
+                "_id": org["_id"],
                 "name": org["name"],
-                "projects": [{"name": project["name"]} for project in projects],
+                "projects": [
+                    {"_id": project["_id"], "name": project["name"]}
+                    for project in projects
+                ],
             }
         )
     return result
