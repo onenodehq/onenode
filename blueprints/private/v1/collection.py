520a6926e19f4a28908e766547d8884f84ad8c9e
TomoKanazawa <itstomohiro@gmail.com>
2024-08-09 20:15:31 +0900

    Update collection endpoints

--- blueprints/private/v1/collection.py
+++ blueprints/private/v1/collection.py
@@ -22,7 +22,6 @@ private_v1_blueprint_collection = Blueprint(
 @requires_auth
 def create_collection():
     data = request.get_json()
-    org_id = data.get("org_id")
     project_id = data.get("project_id")
     collection_name = data.get("collection_name")
     onenode_id = g.onenode_id
@@ -30,7 +29,7 @@ def create_collection():
     if not all([project_id, collection_name]):
         return jsonify({"error": "Missing required fields"}), 400
 
-    if not is_member(onenode_id=onenode_id, org_id=org_id):
+    if not is_member(onenode_id=onenode_id, project_id=project_id):
         return jsonify({"error": "User is not a member of the project"}), 403
 
     create_collection_service(project_id=project_id, collection_name=collection_name)
@@ -58,14 +57,14 @@ def get_collections():
 @private_v1_blueprint_collection.route("", methods=["DELETE"])
 @requires_auth
 def delete_collection():
-    org_id = request.args.get("org_id")
+    project_id = request.args.get("project_id")
     collection_id = request.args.get("collection_id")
     onenode_id = g.onenode_id
 
-    if not all([org_id, collection_id]):
+    if not all([project_id, collection_id]):
         return jsonify({"error": "Missing required fields"}), 400
 
-    if not is_member(onenode_id=onenode_id, org_id=org_id):
+    if not is_member(onenode_id=onenode_id, project_id=project_id):
         return jsonify({"error": "User is not a member of the project"}), 403
 
     collections = delete_collection_service(collection_id=collection_id)
