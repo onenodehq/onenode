e7c18772aaaf710bd341a5e754767bca5061c214
TomoKanazawa <itstomohiro@gmail.com>
2024-08-29 19:06:38 -0700

    Add mongo_operations util file with two utile functions

--- blueprints/v1/org/project/collection/document/services.py
+++ blueprints/v1/org/project/collection/document/services.py
@@ -1,13 +1,13 @@
-from flask import abort
+from blueprints.v1.utils.mongo_operations import (
+    get_collection_or_abort,
+    get_document_ids_by_filter,
+)
 from blueprints.v1.utils.pinecone_operations import pc_client_delete_with_prefixes
-from blueprints.v1.utils.mongo_setup import mongo_client_db
 from celery_tasks import create_documents_task, update_documents_task
 
 
 def create_documents_service(documents: list[dict], namespace: str) -> list[dict]:
-    mongo_collection = mongo_client_db.get_collection(name=namespace)
-    if mongo_collection is None:
-        abort(404, description=f"Collection '{namespace}' not found")
+    get_collection_or_abort(namespace=namespace)
 
     task = create_documents_task.delay(documents=documents, namespace=namespace)
     result = task.id
@@ -16,9 +16,7 @@ def create_documents_service(documents: list[dict], namespace: str) -> list[dict
 
 
 def update_documents_service(filter: dict, update: dict, namespace: str):
-    mongo_collection = mongo_client_db.get_collection(name=namespace)
-    if mongo_collection is None:
-        abort(404, description=f"Collection not found")
+    mongo_collection = get_collection_or_abort(namespace=namespace)
 
     documents_to_update = mongo_collection.find(filter=filter, projection={"_id": 1})
     document_ids: list[str] = [str(document["_id"]) for document in documents_to_update]
@@ -34,12 +32,11 @@ def update_documents_service(filter: dict, update: dict, namespace: str):
 
 
 def delete_documents_service(filter: dict, namespace: str):
-    mongo_collection = mongo_client_db.get_collection(name=namespace)
-    if mongo_collection is None:
-        abort(404, description=f"Collection not found")
+    mongo_collection = get_collection_or_abort(namespace=namespace)
 
-    documents_to_delete = mongo_collection.find(filter=filter, projection={"_id": 1})
-    document_ids: list[str] = [str(document["_id"]) for document in documents_to_delete]
+    document_ids = get_document_ids_by_filter(
+        mongo_collection=mongo_collection, filter=filter
+    )
 
     if not document_ids:
         return
---
+++ blueprints/v1/utils/mongo_operations.py
@@ -0,0 +1,17 @@
+from flask import abort
+from blueprints.v1.utils.mongo_setup import mongo_client_db
+from pymongo.collection import Collection
+
+
+def get_collection_or_abort(namespace: str) -> Collection:
+    # Abort when collection not found
+    mongo_collection = mongo_client_db.get_collection(name=namespace)
+    if mongo_collection is None:
+        abort(404, description=f"Collection '{namespace}' not found")
+    return mongo_collection
+
+
+def get_document_ids_by_filter(mongo_collection: Collection, filter: dict) -> list[str]:
+    documents = mongo_collection.find(filter=filter, projection={"_id": 1})
+    document_ids: list[str] = [str(document["_id"]) for document in documents]
+    return document_ids
