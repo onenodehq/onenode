49bcafce24ca3607904dca0cf70f21da73234283
TomoKanazawa <itstomohiro@gmail.com>
2024-09-28 10:04:14 -0700

    Add list_collections endpoint

--- blueprints/private/v1/org/project/db/routes.py
+++ blueprints/private/v1/org/project/db/routes.py
@@ -10,18 +10,3 @@ private_v1_blueprint_db = Blueprint(
 )
 
 private_v1_blueprint_db.register_blueprint(private_v1_blueprint_collection)
-
-
-""" @private_v1_blueprint_db.route("/<string:db_id>", methods=["GET"])
-@requires_auth
-def list_collections(org_id, project_id, db_id):
-    onenode_id = g.onenode_id
-
-    check_permission(onenode_id=onenode_id, project_id=project_id)
-
-    collections = list_collections_service(project_id=project_id)
-
-    return json_util.dumps(collections), 200
- """
-
-## db_name should not have spaces
