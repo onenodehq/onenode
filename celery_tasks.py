6cbd5cc6959103643fcad06b61384191aeb04b3f
TomoKanazawa <itstomohiro@gmail.com>
2024-10-21 22:54:29 -0700

    Add missing namespace to pc upsert

--- celery_tasks.py
+++ celery_tasks.py
@@ -4,12 +4,12 @@ from celery_setup import celery
 
 
 @celery.task(bind=True)
-def save_vectors_task(self, vector_bases: list):
+def save_vectors_task(self, vector_bases: list, project_id: str, db_name):
     vectors = []
     for vector_basis in vector_bases:
         embedding = embed_text(vector_basis["values"])
         vector_basis.update({"values": embedding})
         vectors.append(vector_basis)
-    pc_upsert(vectors)
+    pc_upsert(vectors, project_id, db_name)
 
     return
