fd140d9dfbf061f9036a3feb6e37d4a242ef101a
TomoKanazawa <itstomohiro@gmail.com>
2025-04-06 12:58:07 -0700

    Add private query endpoint

--- blueprints/private/org/project/db/collection/documents/routes.py
+++ blueprints/private/org/project/db/collection/documents/routes.py
@@ -5,6 +5,7 @@ from bson import json_util, ObjectId
 from blueprints.private.org.project.db.collection.documents.services import list_documents_service
 from blueprints.v0.db.collection.document.find.services import find_docs_service
 from blueprints.private.services import check_project_permission
+from blueprints.v0.db.collection.document.query.services import query_chunks_service
 from blueprints.v0.db.collection.document.services import delete_docs_service, update_docs_service
 from blueprints.v0.utils.mongo_operations import get_client_collection
 from blueprints.v0.utils.mongo_setup import mongo_orgs
@@ -175,3 +176,44 @@ def update_docs(
     )
 
     return json_util.dumps(result), 200
+
+@private_blueprint_document.route("/query", methods=["POST"])
+@requires_auth
+@require_admin_api_key
+def query_documents(org_id, project_id, db_name, collection_name):
+    user_id = g.user_id
+
+    check_project_permission(user_id, org_id, project_id)
+
+    # Get organization to access plan
+    org = mongo_orgs.find_one({"_id": ObjectId(org_id)})
+    if org and "plan" in org:
+        g.plan = org["plan"].get("type", "free")
+    else:
+        g.plan = "free"
+    
+    data = request.get_json() 
+    filter = data.get("filter")
+    projection = data.get("projection")
+    query = data.get("query")
+    top_k = int(data.get("top_k", 10))
+    emb_model = data.get("emb_model", "text-embedding-3-small")
+    
+    # Get documents using find_docs_service
+    data = query_chunks_service(
+        project_id,
+        db_name,
+        collection_name,
+        query,
+        filter,
+        top_k,
+        projection,
+        False,
+        emb_model,
+    )
+    
+    response = {
+        "matches": data,
+    }
+
+    return json_util.dumps(response), 200
