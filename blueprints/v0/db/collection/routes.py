ab4523b55abb92e68414c6b95d8b034184b46bc7
TomoKanazawa <itstomohiro@gmail.com>
2025-06-06 20:25:12 -0700

    Add anon collection drop endpoint

--- blueprints/v0/db/collection/routes.py
+++ blueprints/v0/db/collection/routes.py
@@ -4,12 +4,14 @@ from blueprints.v0.db.collection.document.routes import v0_blueprint_doc
 from blueprints.v0.utils.api_key_permissions import check_api_key_permissions
 from blueprints.v0.utils.mongo_operations import split_db_id
 from blueprints.v0.db.collection.services import delete_collection_service
+from blueprints.v0.db.collection.aonon_routes import v0_blueprint_collection_anon
 
 v0_blueprint_collection = Blueprint(
     "v0_collection", __name__, url_prefix="/<string:db_id>/collection"
 )
 
 v0_blueprint_collection.register_blueprint(v0_blueprint_doc)
+v0_blueprint_collection.register_blueprint(v0_blueprint_collection_anon)
 
 @v0_blueprint_collection.route("/<string:collection_name>", methods=["DELETE"])
 @require_api_key
