73d36fccd6bcfb0fb1e9cfdbae298aa9488a9960
TomoKanazawa <itstomohiro@gmail.com>
2025-08-23 13:19:15 -0700

    Delete anon endpoints

--- blueprints/v0/project/anon_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from blueprints.v0.project.db.anon_routes import v0_blueprint_anon_db
-
-v0_blueprint_anon_project = Blueprint(
-    "v0_anon_project", __name__, url_prefix="/anon-project"
-)
-
-v0_blueprint_anon_project.register_blueprint(v0_blueprint_anon_db)
--- blueprints/v0/routes.py
+++ blueprints/v0/routes.py
@@ -1,8 +1,6 @@
 from flask import Blueprint
 from blueprints.v0.project.routes import v0_blueprint_project
-from blueprints.v0.project.anon_routes import v0_blueprint_anon_project
 
 v0_blueprint_root = Blueprint("v0_root", __name__, url_prefix="/v0")
 
 v0_blueprint_root.register_blueprint(v0_blueprint_project)
-v0_blueprint_root.register_blueprint(v0_blueprint_anon_project)
