33c84fcd32915483218221a2b74b620c3f93dd08
TomoKanazawa <itstomohiro@gmail.com>
2025-08-24 10:13:26 -0700

    Add local MongoDB

--- blueprints/v0/utils/mongo_setup.py
+++ blueprints/v0/utils/mongo_setup.py
@@ -1,7 +1,6 @@
 import os
-import certifi
 from pymongo import MongoClient
 
 
 MONGO_CLIENT_URI = os.getenv("MONGO_CLIENT_URI")
-mongo_client_cluster = MongoClient(MONGO_CLIENT_URI, tlsCAFile=certifi.where())
+mongo_client_cluster = MongoClient(MONGO_CLIENT_URI)
