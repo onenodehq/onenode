7e035956fa0ef795cea130ff106c4cb51883cc0e
TomoKanazawa <itstomohiro@gmail.com>
2025-08-24 09:41:20 -0700

    Delete unsused Mongo admin logics and vars

--- blueprints/v0/project/db/collection/services.py
+++ blueprints/v0/project/db/collection/services.py
@@ -1,4 +1,4 @@
-from blueprints.v0.utils.mongo_operations import get_client_collection, unregister_collection
+from blueprints.v0.utils.mongo_operations import get_client_collection
 from blueprints.v0.utils.pinecone_operations import pc_client_delete_collection
 from blueprints.v0.utils.minio_operations import minio_delete_collection
 
@@ -7,7 +7,6 @@ def delete_collection_service(project_id: str, db_name: str, collection_name: st
     drop_client_collection(project_id, db_name, collection_name)
     pc_client_delete_collection(project_id, db_name, collection_name)
     minio_delete_collection(project_id, db_name, collection_name)
-    unregister_collection(project_id, db_name, collection_name)
     return True
 
 def drop_client_collection(project_id: str, db_name: str, collection_name: str):
--- blueprints/v0/utils/mongo_setup.py
+++ blueprints/v0/utils/mongo_setup.py
@@ -5,13 +5,3 @@ from pymongo import MongoClient
 
 MONGO_CLIENT_URI = os.getenv("MONGO_CLIENT_URI")
 mongo_client_cluster = MongoClient(MONGO_CLIENT_URI, tlsCAFile=certifi.where())
-
-MONGO_ADMIN_URI = os.getenv("MONGO_ADMIN_URI")
-MONG0_FREE_STORAGE_LIMIT_MB = int(os.getenv("MONG0_FREE_STORAGE_LIMIT_MB"))
-mongo_admin_cluster = MongoClient(MONGO_ADMIN_URI, tlsCAFile=certifi.where())
-mongo_admin_db = mongo_admin_cluster.get_database("admin_db")
-mongo_users = mongo_admin_db.get_collection("users")
-mongo_api_keys = mongo_admin_db.get_collection("api_keys")
-mongo_orgs = mongo_admin_db.get_collection("orgs")
-mongo_usage = mongo_admin_db.get_collection("usage")
-mongo_stats = mongo_admin_db.get_collection("stats")
